5d679bf724a0dfe11c610f7f26b68ff2533e95f0
amiceli <antoine@miceli.click>
2026-06-14 16:53:44 +0200

    test: tmdb controller

--- src/__tests__/helpers/create-test-app.ts
+++ src/__tests__/helpers/create-test-app.ts
@@ -6,6 +6,7 @@ import type { App } from 'supertest/types'
 import type { DataSourceOptions } from 'typeorm'
 import { createPgMemDataSource } from '@/__tests__/pg-mem-data-source'
 import { AppModule } from '@/app.module'
+import { TmdbDetails } from '@/tmdb/entities/tmdb-details.entity'
 import { Channel } from '@/xml-tv/entities/channel.entity'
 import { Program } from '@/xml-tv/entities/program.entity'
 
@@ -19,7 +20,7 @@ export const createTestApp = async (): Promise<{ app: INestApplication<App>; mod
             factory: (options: DataSourceOptions) =>
                 createPgMemDataSource({
                     ...options,
-                    entities: [Channel, Program],
+                    entities: [Channel, Program, TmdbDetails],
                 } as DataSourceOptions),
         })
         .compile()
